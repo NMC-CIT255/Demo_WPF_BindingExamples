7604fbcf594339afbcd68f8cc847709327f39a33
John Velis <john@johnvelis.com>
2018-11-06 12:01:05 -0500

    initial publish

---
+++ ElementToElement/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ElementToElement
+{
+    class Person : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set {
+                _age = value;
+                var pc = PropertyChanged;
+                if (pc != null)
+                {
+                    pc(this, new PropertyChangedEventArgs("Age"));
+                }
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
